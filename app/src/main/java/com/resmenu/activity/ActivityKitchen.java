5626a28ad5d01d016b8b0d6337a2c79ffff198de
rahul <rahulbibave@gmail.com>
2019-06-12 18:37:47 +0530

    Timmer added

--- app/src/main/java/com/resmenu/activity/ActivityKitchen.java
+++ app/src/main/java/com/resmenu/activity/ActivityKitchen.java
@@ -59,7 +59,7 @@ public class ActivityKitchen extends AppCompatActivity {
                 ConfirmOrder();
             }
         });
-        mBtnMinimize.setOnClickListener(ginew View.OnClickListener() {
+        mBtnMinimize.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 finish();
